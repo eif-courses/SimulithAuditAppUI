84ff80a19c8d1d1ee888a9d0ce5404a2df7625ea
Marius Gžegoževskis <m.gzegozevskis@ad.viko.lt>
2022-03-18 10:31:05 +0200

    update v2

--- SimulithAuditAzureUI/Program.cs
+++ SimulithAuditAzureUI/Program.cs
@@ -1,19 +1,13 @@
-using SimulithAuditAppUI;
+using SimulithAuditAzureUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureServices();
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
   app.UseExceptionHandler("/Error");
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -26,10 +20,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
---
+++ SimulithAuditAzureUI/RegisterServices.cs
@@ -0,0 +1,23 @@
+﻿namespace SimulithAuditAzureUI
+{
+  public static class RegisterServices
+  {
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+      // Add services to the container.
+     
+      builder.Services.AddRazorPages();
+      builder.Services.AddServerSideBlazor();
+      builder.Services.AddMemoryCache(); // Caching
+
+      // Scoped once per user 
+      // Transient 
+      // Sharing data
+      builder.Services.AddSingleton<IDbConnection, DbConnection>();
+      builder.Services.AddSingleton<ICategoryData, MongoCategoryData>(); 
+      builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+      builder.Services.AddSingleton<IInternalAuditData, MongoInternalAuditData>();
+      builder.Services.AddSingleton<IUserData, MongoUserData>();
+    }
+  }
+}
